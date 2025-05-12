e7b866729ca01ac30a6f80ba032164b583ac542e
Andreas Zulus <andreas.zulus@me.com>
2025-05-12 15:59:38 +0200

    revert api

--- main.tf
+++ main.tf
@@ -9,14 +9,3 @@ provider "google" {
   project = var.project
   region = var.region
 }
-
-resource "google_project_service" "multiple_apis" {
-  for_each = toset([
-    "cloudresourcemanager.googleapis.com",
-    "serviceusage.googleapis.com",
-    "storage.googleapis.com",
-    "compute.googleapis.com",
-    "container.googleapis.com",
-  ])
-  service                    = each.value
-}
